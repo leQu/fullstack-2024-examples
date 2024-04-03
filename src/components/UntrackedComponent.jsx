920eb61c94da2c8efc5a868be52cec6c400f92e4
Alex <alex.w.lantz@gmail.com>
2024-04-03 10:56:37 +0200

    Added conflicts

--- src/components/UntrackedComponent.jsx
+++ src/components/UntrackedComponent.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const UntrackedComponent = () => {
   return (
     <div>
-      <h1>Hello, I'm the UntrackedComponent!</h1>
+      <h1>Hello, I'm the Tracked component</h1>
+      <p>I'm very happy about that 🤗</p>
     </div>
   );
 };
