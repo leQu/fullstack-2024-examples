f695ebe7f5ead2e1acef82e83971cfa5414c8c01
Alex <alex.w.lantz@gmail.com>
2024-04-03 10:01:35 +0200

    In this commit changes to the project name and an untracked component was added

---
+++ src/components/UntrackedComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UntrackedComponent = () => {
+  return (
+    <div>
+      <h1>Hello, I'm the UntrackedComponent!</h1>
+    </div>
+  );
+};
+
+export default UntrackedComponent;
