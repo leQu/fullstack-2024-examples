ffc810e82635188a6f90d209a8e8104e04a2e0df
Alex <alex.w.lantz@gmail.com>
2024-04-05 11:10:39 +0200

    Updated app with more components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import moment from "moment";
 
 import logo from "./logo.svg";
@@ -6,29 +7,40 @@ import WordListComponent from "./components/WordListComponent";
 import "./App.css";
 import MySmartComponent from "./components/MySmartComponent";
 import UntrackedComponent from "./components/UntrackedComponent";
+import CustomButton from "./components/CustomButton";
+
+export const CounterContext = React.createContext();
+export const CounterContextUpdater = React.createContext();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <MainHeader title="Hej alla" description="Detta är vårt nya projekt" />
-        <UntrackedComponent />
-        <MySmartComponent initialCount={100} />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <WordListComponent words={["banan", "melon", "kiwi", "citron"]} />
-          Learn React {moment().format("dd YYYY mm")}
-        </a>
-      </header>
-    </div>
+    <CounterContext.Provider value={88}>
+      <CounterContextUpdater.Provider value={88}>
+        <div className="App">
+          <header className="App-header">
+            <MainHeader description="Detta är vårt nya projekt" />
+            <UntrackedComponent />
+            <MySmartComponent initialCount={100} />
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <WordListComponent words={["banan", "melon", "kiwi", "citron"]} />
+              Learn React {moment().format("dd YYYY mm")}
+            </a>
+            <CustomButton onClick={() => console.log("Klick!")}>
+              Klicka här
+            </CustomButton>
+          </header>
+        </div>
+      </CounterContextUpdater.Provider>
+    </CounterContext.Provider>
   );
 }
 
